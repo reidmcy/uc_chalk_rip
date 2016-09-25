e63a509fc70eaac26682e47853f5f0954763f3b0
Reid McIlroy-Young <reid@reidmcy.com>
2016-09-24 23:16:01 -0500

    Adding pypi stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+version = '0.1'
+
 setup(name='uc-chalk-rip',
-    version = '0.1',
+    version = version,
     description = "Ripper for Uchicago's chalk site",
     author="Reid McIlroy-Young",
     author_email = "reidmcy@uchicago.edu",
     license = 'GPL',
+    url="https://github.com/reidmcy/uc_chalk_rip",
+    download_url = "https://github.com/reidmcy/uc_chalk_rip/archive/{}.tar.gz".format(version),
     packages = find_packages(),
     entry_points={'console_scripts': [
               'chalk_rip = chalk_rip:main',

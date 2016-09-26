99a7c2ddccf2b112dde40b8726d3e59d9dcbf276
Reid McIlroy-Young <reid@reidmcy.com>
2016-09-26 15:37:09 -0500

    Added clasifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.4'
+version = '0.5'
 
 setup(name='uc_chalk_rip',
     version = version,
@@ -9,6 +9,10 @@ setup(name='uc_chalk_rip',
     author_email = "reidmcy@uchicago.edu",
     license = 'GPL',
     url="https://github.com/reidmcy/uc_chalk_rip",
+    classifiers = [
+    "Programming Language :: Python :: 3 :: Only",
+    "Development Status :: 4 - Beta",
+    ],
     download_url = "https://github.com/reidmcy/uc_chalk_rip/archive/{}.tar.gz".format(version),
     packages = find_packages(),
     install_requires= ['bs4', 'requests'],
